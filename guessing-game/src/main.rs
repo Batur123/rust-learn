d6d26ae8de05f5897dfce1cc8f7cc76ed943bfee
Batuhan Özkoç <batur.ozkoc@gmail.com>
2024-02-22 14:25:00 +0200

    hotfix(print): Fix print guess

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -30,7 +30,7 @@ fn main() {
             },
         };
 
-        println!("You guessed: {guess}");
+        println!("You guessed: {}", guess);
 
         match guess.cmp(&random_number) {
             Ordering::Less => {
